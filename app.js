51bf15d1a512c11f05cc115cd92635b90bd936de
Doni Setiawan <donisetiawan.050@gmail.com>
2019-12-14 14:32:03 +0700

    example02

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require('express');
 
 const app = express();
+const stack = [];
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.post('/stack', (req, res, next) => {
+  let buffer = '';
+
+  req.on('data', (data) => {
+    buffer += data;
+  });
+  req.on('end', () => {
+    stack.push(buffer);
+    return next();
+  });
+});
+
+app.delete('/stack', (req, res, next) => {
+  stack.pop();
+  return next();
+});
+
+app.get('/stack/:index', (req, res) => {
+  if (
+    req.params.index >= 0
+    && req.params.index < stack.length
+  ) {
+    return res.end(`${stack[req.params.index]}`);
+  }
+  res.status(404).end();
+});
+
+app.use('/stack', (req, res) => {
+  res.send(stack);
 });
 
 app.listen(3000);
